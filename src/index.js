576c50f379acb0dbcd2102eb33c16605272d6620
Morgován Nimród <morgovannimrod22@gmail.com>
2022-10-11 11:05:40 +0200

    Tömb generálása és szűrése

--- src/index.js
+++ src/index.js
@@ -10,4 +10,15 @@ let tomb = [];
 for (let i = 0; i < 30; i++) {
     tomb.push(Math.floor(Math.random()* 30 + 1));
 }
+tomb.forEach((e) => {
+    console.log(e);
+});
+
+let ottelOszthato = tomb.filter(e => e % 5 == 0);
+console.log(ottelOszthato);
+
+ottelOszthato.forEach((e) => {
+    console.log(e);
+});
+
 });
